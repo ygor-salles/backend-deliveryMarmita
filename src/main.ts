99526084580c89ce9401696e6d2f57c73ef545e6
unknown <ygorsallesaniceto@gmail.com>
2021-10-31 19:15:52 -0300

    corrigindo arquivo procfile

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,9 @@ async function bootstrap() {
       },
     }),
   );
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
